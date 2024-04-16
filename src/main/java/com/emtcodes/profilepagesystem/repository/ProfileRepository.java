e506953478125d0ddeaa20c89a42a35f9ac615ef
Emma Mandat-Toland <etoland007@gmail.com>
2024-04-16 15:47:40 +0100

    chore: connect to mySql

--- src/main/java/com/emtcodes/profilepagesystem/repository/ProfileRepository.java
+++ src/main/java/com/emtcodes/profilepagesystem/repository/ProfileRepository.java
@@ -2,7 +2,9 @@ package com.emtcodes.profilepagesystem.repository;
 
 import com.emtcodes.profilepagesystem.model.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
 
 }
